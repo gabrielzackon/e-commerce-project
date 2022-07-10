7c63398def3609fe606dad267a54266689eb0999
Or Zachchinsky <ozzachch@amazon.com>
2022-07-10 16:19:01 +0300

    updated products list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Football Shirts</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <div className="product-image">
+                <img src={product.image} alt={product.name} />
+              </div>
+              <div className="product-info">
+                <p className="product-name">{product.name}</p>
+                <p className="product-price">{product.price}$</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
