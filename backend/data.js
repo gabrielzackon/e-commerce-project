3ed5f11430f221d53f3f53feed01257a47bae3e3
Or Zachchinsky <ozzachch@amazon.com>
2022-07-18 18:51:16 +0300

    changed images size to 600X600

--- backend/data.js
+++ backend/data.js
@@ -4,7 +4,7 @@ const data = {
       name: 'Manchester United Home kit 2022/2023',
       slug: 'man-utd-home-kit-22-23',
       category: 'Premier League',
-      image: '/images/man-utd-home.png', // 400px X 400px
+      image: '/images/man-utd-home.png', // 600 X 600 pixels
       price: 120,
       countInStock: 10,
       brand: 'Adidas',
@@ -16,7 +16,7 @@ const data = {
       name: 'Manchester United Away kit 2022/2023',
       slug: 'man-utd-away-kit-22-23',
       category: 'Premier League',
-      image: '/images/man-utd-away.png', // 400px X 400px
+      image: '/images/man-utd-away.png', // 600 X 600 pixels
       price: 120,
       countInStock: 10,
       brand: 'Adidas',
@@ -28,7 +28,7 @@ const data = {
       name: 'FC Barcelona Home kit 2022/2023',
       slug: 'fc-barcelona-home-kit-22-23',
       category: 'La Liga',
-      image: '/images/barcelona-home.png', // 400px X 400px
+      image: '/images/barcelona-home.png', // 600 X 600 pixels
       price: 110,
       countInStock: 0,
       brand: 'Nike',
@@ -40,7 +40,7 @@ const data = {
       name: 'FC Barcelona Away kit 2022/2023',
       slug: 'fc-barcelona-away-kit-22-23',
       category: 'La Liga',
-      image: '/images/barcelona-away.png', // 400px X 400px
+      image: '/images/barcelona-away.png', // 600 X 600 pixels
       price: 110,
       countInStock: 10,
       brand: 'Nike',
