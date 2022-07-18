1bc6e836016f11beff6e2bd29e784484f4081b75
Gabriel Zackon <gabrielzackon@h-MacBook-Pro-sl-Gabriel.local>
2022-07-18 11:43:28 +0300

    created backend server

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Manchester United Home kit 2022/2023',
+      slug: 'man-utd-home-kit-22-23',
+      category: 'Premier League',
+      image: '/images/man-utd-home.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.7,
+      numReviews: 10,
+      description: 'Manchester United Home kit 2022/2023',
+    },
+    {
+      name: 'Manchester United Away kit 2022/2023',
+      slug: 'man-utd-away-kit-22-23',
+      category: 'Premier League',
+      image: '/images/man-utd-away.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.7,
+      numReviews: 10,
+      description: 'Manchester United Away kit 2022/2023',
+    },
+    {
+      name: 'FC Barcelona Home kit 2022/2023',
+      slug: 'fc-barcelona-home-kit-22-23',
+      category: 'La Liga',
+      image: '/images/barcelona-home.png', // 679px × 829px
+      price: 110,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.6,
+      numReviews: 10,
+      description: 'FC Barcelona Home kit 2022/2023',
+    },
+    {
+      name: 'FC Barcelona Away kit 2022/2023',
+      slug: 'fc-barcelona-away-kit-22-23',
+      category: 'La Liga',
+      image: '/images/barcelona-away.png', // 679px × 829px
+      price: 110,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.6,
+      numReviews: 10,
+      description: 'FC Barcelona Away kit 2022/2023',
+    },
+  ],
+};
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
