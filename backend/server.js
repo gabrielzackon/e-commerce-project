6cbc4e6e4d9fd32efabfdcc6043a2204f578f2fb
Gabriel Zackon <gabrielzackon@h-MacBook-Pro-sl-Gabriel.local>
2022-07-18 16:38:24 +0300

    bootstrap + change header

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from 'express';
 import data from './data.js';
 
 const app = express();
-const port = process.env.PORT || 5050;
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+const port = process.env.PORT || 5050;
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">Football Shirts</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Football Shirts</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
