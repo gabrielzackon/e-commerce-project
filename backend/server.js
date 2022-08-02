f590048f56e0f6f198abf97bc2fd425bf593ad12
Or Zachchinsky <ozzachch@amazon.com>
2022-08-02 17:22:51 +0300

    created user router and generate token per user

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,10 +17,20 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 const port = process.env.PORT || 5050;
+
+app.use((error, req, res, next) => {
+  res.status(500).send({ message: error.message });
+});
+
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
